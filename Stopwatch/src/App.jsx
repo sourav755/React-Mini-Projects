6428fc7d9aeb3c8c5d054c80e59796913704f311
Sourav <garaisourav143@gmail.com>
2024-05-20 22:08:09 +0530

    ready

--- Stopwatch/src/App.jsx
+++ Stopwatch/src/App.jsx
@@ -21,21 +21,36 @@ function App() {
   return (
     <>
       <div className="flex flex-col text-center justify-center py-8">
-        <h1 className="text-2xl">Stopwatch</h1>
-        <div>
+        <h1 className="text-2xl font-semibold pb-2">Stopwatch</h1>
+        <div className="text-xl font-semibold py-4">
           <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
           <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
           <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
         </div>
 
-        <div>
+        <div className="flex flex-row justify-center gap-4">
           {running ? (
-            <button onClick={() => setRunning(false)}>Stop</button>
+            <button
+              className="border-2 rounded-lg py-1 px-3.5"
+              onClick={() => setRunning(false)}
+            >
+              Stop
+            </button>
           ) : (
-            <button onClick={() => setRunning(true)}>Start</button>
+            <button
+              className="border-2 rounded-lg py-1 px-3"
+              onClick={() => setRunning(true)}
+            >
+              Start
+            </button>
           )}
 
-          <button onClick={() => setTime(0)}>Reset</button>
+          <button
+            className="border-2 rounded-lg py-1 px-3.5"
+            onClick={() => setTime(0)}
+          >
+            Reset
+          </button>
         </div>
       </div>
     </>

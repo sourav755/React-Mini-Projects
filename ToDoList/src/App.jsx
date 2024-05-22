757e62aec29f04fb30eea9a812385ba92bf0c386
Sourav <garaisourav143@gmail.com>
2024-05-23 00:00:21 +0530

    logic building

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Input from "./components/Input";
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  console.log(taskList);
   return (
-    <>
-      <h1>ToDo List</h1>
-      <Input></Input>
-    </>
+    <div className="flex flex-col items-center justify-center py-12 gap-4">
+      <h1 className="text-xl font-semibold">ToDo List</h1>
+      <Input taskList={taskList} setTaskList={setTaskList}></Input>
+      <div>
+        {taskList.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </div>
+    </div>
   );
 }
 
--- ToDoList/src/components/Input.jsx
+++ ToDoList/src/components/Input.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import React from "react";
 
-function input() {
+function input({ taskList, setTaskList }) {
+  const [input, setInput] = useState("");
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    setTaskList([...taskList, input]);
+    setInput("");
+  };
+
   return (
     <>
-      <h2>Add Task</h2>
+      <form className="flex flex-row items-center gap-3">
+        <input
+          className="border rounded-lg py-1.5 px-2.5 text-lg"
+          type="text"
+          placeholder="Add a Task"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          className="bg-violet-400 text-white py-2 px-4 rounded"
+          onClick={handleAddTask}
+        >
+          Add
+        </button>
+      </form>
     </>
   );
 }
